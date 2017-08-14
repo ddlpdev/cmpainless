084f8bfd6a64eeba88d9b2db48d579cf73454433
David de la Pena <dave@Davids-MacBook-Pro.local>
2017-08-14 15:26:49 +0100

    adding mongo mlarky

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // server.js
 const express = require('express');
 const path = require('path');
+var mongodb = require("mongodb");
 
 
+var ObjectID = mongodb.ObjectID;
 
 const app = express();
 
@@ -27,33 +29,29 @@ const forceSSL = function() {
 // middleware
 app.use(forceSSL());
 
+//setup DB
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
 
 
-
-
-  
-
-
-// CONTACTS API ROUTES BELOW
-
-
-// CONTACTS API ROUTES BELOW
-
-// // Generic error handler used by all endpoints.
-// function handleError(res, reason, message, code) {
-//   console.log("ERROR: " + reason);
-//   res.status(code || 500).json({"error": message});
-// }
-
-// /*  "/api/waffle"
-//  *    GET: returns waffle
- 
-//  */
-
-// app.get("/api/waffle", function(req, res) {
-//  res.send('test\n');
-// });
-
+//use api app
 app.use('/api', api);
 
 
