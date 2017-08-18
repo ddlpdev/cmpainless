4d7fe3540689fe6bf92fe1b74eb5769570881543
David de la Pena <dave@Davids-MacBook-Pro.local>
2017-08-18 13:06:05 +0100

    updating api

--- server.js
+++ server.js
@@ -45,34 +45,34 @@ var db;
 
 // const port = process.env.PORT || 3000;
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI, { promiseLibrary: Promise })
-  .catch(err => console.error(err.stack))
-  .then(db => {
-    app.locals.db = db;
-    app.listen(process.env.PORT || 8080, () => {
-        //var port = server.address().port;
-      console.log(`Node.js app is listening`);
-    });
-  });
-
-
-// Connect to the database before starting the application server.
-//  mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-//   if (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
-
-//   // Save database object from the callback for reuse.
-//   db = database;
-//   console.log("Database connection ready");
-
-//   //Initialize the app.
-//   var server = app.listen(process.env.PORT || 8080, function () {
-//     var port = server.address().port;
-//     console.log("App now running on port", port);
+// mongodb.MongoClient.connect(process.env.MONGODB_URI, { promiseLibrary: Promise })
+//   .catch(err => console.error(err.stack))
+//   .then(db => {
+//     app.locals.db = db;
+//     app.listen(process.env.PORT || 8080, () => {
+//         //var port = server.address().port;
+//       console.log(`Node.js app is listening`);
+//     });
 //   });
-// });
+
+
+//Connect to the database before starting the application server.
+ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  //Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
 
 
 
