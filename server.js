7b71d6b53494f4242067eda707822e80ad8ca168
David de la Pena <dave@Davids-MacBook-Pro.local>
2017-08-18 12:22:12 +0100

    updating api

--- server.js
+++ server.js
@@ -43,35 +43,35 @@ app.use(express.static(__dirname + '/dist'));
 //setup DB
 var db;
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-mongodb.MongoClient.connect(process.env.MONGODB_URIurl, { promiseLibrary: Promise })
-  .catch(err => console.error(err.stack))
-  .then(db => {
-    app.locals.db = db;
-    app.listen(process.env.PORT||8080, () => {
-      console.log(`Node.js app is listening at http://localhost:${port}`);
-    });
-  });
+// mongodb.MongoClient.connect(process.env.MONGODB_URIurl, { promiseLibrary: Promise })
+//   .catch(err => console.error(err.stack))
+//   .then(db => {
+//     app.locals.db = db;
+//     app.listen(process.env.PORT||8080, () => {
+//       console.log(`Node.js app is listening at http://localhost:${port}`);
+//     });
+//   });
 
 
 // Connect to the database before starting the application server.
 // mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-//   if (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-//   // Save database object from the callback for reuse.
-//   db = database;
-//   console.log("Database connection ready");
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
 
-//   //Initialize the app.
-//   var server = app.listen(process.env.PORT || 8080, function () {
-//     var port = server.address().port;
-//     console.log("App now running on port", port);
-//   });
-// });
+  //Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
 
 
 
